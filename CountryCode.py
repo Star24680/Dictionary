5c914598c523783cb55548c5efcffdc2fbf5f165
Star24680 <gulerumaan1@gmail.com>
2024-11-05 18:09:47 +0500

    INtitialCommit

---
+++ CountryCode.py
@@ -0,0 +1,5 @@
+CountryCode={'India':'0091','Australia':'0025','Pakistan':'0092'}
+print("CountryCode for India")
+print(CountryCode.get('India','Not Found'))
+print("CountryCode for japan")
+print(CountryCode.get('Japan','Not Found'))   
